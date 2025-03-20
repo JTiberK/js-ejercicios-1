929d6c23de521022cd63cc861e8f71271ea8ea80
Juan Carlos Sánchez Cifuentes <mariano.navarro@tajamar365.com>
2025-03-20 13:55:09 +0100

    Actualizado Ejercicios1.js hasta la linea 123

--- scripts/Ejercicios1.js
+++ scripts/Ejercicios1.js
@@ -19,7 +19,7 @@ console.log(typeof "JavaScript"); // Salida: "string"
 
 console.log(typeof true); // Salida: "boolean"
 
-/******************* EJERCICIO 44 *************************************/
+/******************* EJERCICIO 4 *************************************/
 
 const resultado = (15 + 3) * (8 / 2);
 
@@ -73,3 +73,50 @@ const result = textMayus + " desde JS";
 console.log(result); // Salida: HOLA MUNDO desde JS
 
 /******************* EJERCICIO 9 *************************************/
+
+const frutas = ["manzana", "banana", "pera"];
+
+const segundaFruta = frutas[1];
+
+console.log(segundaFruta); // Salida: banana
+
+/******************* EJERCICIO 10 *************************************/
+
+let frut= ["manzana"];
+
+// Muestra el estado inicial del array
+console.log("Estado inicial:", frut); // Salida: Estado inicial: [ 'manzana' ]
+
+// Usa push para añadir "naranja" al array
+frut.push("naranja");
+
+// Muestra el estado del array después de usar push
+console.log("Después de push:", frut); // Salida: Después de push: [ 'manzana', 'naranja' ]
+
+
+console.log("Después de pop:", frut); // Salida: Después de pop: [ 'manzana' ]
+
+/******************* EJERCICIO 11 *************************************/
+
+let usuario = {
+  nombre: "Juan Pérez",
+  edad: 30,
+};
+
+// Muestra el valor de las propiedades en la consola
+console.log("Nombre:", usuario.nombre);  // Salida: Nombre: Juan Pérez
+console.log("Edad:", usuario.edad);  // Salida: Edad: 30
+
+/******************* EJERCICIO 12 *************************************/
+
+// Se añade un método saludar al objeto usuario
+usuario = {
+  saludar: function() {
+    console.log("Hola");
+  }
+};
+
+// Llama al método saludar
+usuario.saludar(); // Salida: Hola
+
+/******************* EJERCICIO 13 *************************************/
